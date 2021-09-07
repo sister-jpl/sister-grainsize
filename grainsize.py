72ec816de4d7aa697322b2e015908e3dbf7ad099
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2021-09-07 09:20:21 -0400

    Updated variable names and description for consistency

--- grainsize.py
+++ grainsize.py
@@ -49,36 +49,24 @@ def main():
 
     desc = "Estimate snow grain size"
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('input', type=str,
+    parser.add_argument('rfl_file', type=str,
                         help='Input reflectance image')
     parser.add_argument('out_dir', type=str,
                         help='Output directory')
-    parser.add_argument('--root_dir', action='store',
-                        help='Root of source directory',
-                        default=os.path.realpath(os.path.split(__file__)[0]))
     parser.add_argument('--verbose',      action='store_true')
     parser.add_argument('--mask', type=str, default=None,
-                        help='External snow mask')
-
+                        help='External snow mask path')
     args = parser.parse_args()
 
-    #Testing
-    # parser = argparse.ArgumentParser()
-    # args = parser.parse_args([])
-    # args.input = '/data2/prisma/l2/PRS_20210302180507_20210302180511_0001_srtm/PRS_20210302180507_20210302180511_0001_rfl_prj_rfl_10nm'
-    # args.out_dir ='/data2/temp/snow_testing/'
-    # args.root_dir = '/home/adam/Dropbox/rs/sister/repos/sister-grainsize/'
-    # args.mask = '/data2/prisma/l2/PRS_20210302180507_20210302180511_0001_srtm/PRS_20210302180507_20210302180511_0001_rdn_prj_cls'
-    # args.verbose = True
-
     # Load band depth and snow size data
-    interp_data = np.loadtxt("%s/data/grainsize_v_bandarea_nolin_dozier_2000_interp.csv" % args.root_dir,
+    root_dir =os.path.realpath(os.path.split(__file__)[0])
+    interp_data = np.loadtxt("%s/data/grainsize_v_bandarea_nolin_dozier_2000_interp.csv" % root_dir,
                              delimiter = ',').T
     interpolator = interp1d(interp_data[0],interp_data[1],
                             kind = 'cubic',fill_value ='extrapolate')
 
-    img = htl.HyTools()
-    img.read_file(args.input,'envi')
+    reflectance = htl.HyTools()
+    reflectance.read_file(args.rfl_file,'envi')
 
     # Use external mask
     if args.mask:
@@ -87,26 +75,25 @@ def main():
         mask = msk.get_band(0)==2
     # Use NDSI threshold for mask
     else:
-        mask = img.ndi(670,1500) < .9
-        mask[~img.mask['no_data']] =False
+        mask = reflectance.ndi(670,1500) < .9
+        mask[~reflectance.mask['no_data']] =False
 
     # Get continuum start and end bands
     wave1, wave2  = 950,1080
-    wave1_ind = img.wave_to_band(wave1)
-    wave2_ind = img.wave_to_band(wave2)
+    wave1_ind = reflectance.wave_to_band(wave1)
+    wave2_ind = reflectance.wave_to_band(wave2)
 
-    grain_size = np.full((img.lines,img.columns),-1)
-    iterator =img.iterate(by = 'chunk',chunk_size = (200,200))
+    grain_size = np.full((reflectance.lines,reflectance.columns),-1)
+    iterator =reflectance.iterate(by = 'chunk',chunk_size = (200,200))
     i = 0
 
     while not iterator.complete:
-
         chunk = iterator.read_next()[:,:,wave1_ind:wave2_ind+1]
 
         # Calculate continuum removed spectrum
         slope = (chunk[:,:,-1]-chunk[:,:,0])/(wave2-wave1)
         intercept = chunk[:,:,-1]- wave2*slope
-        continuum = np.einsum('i,mj->mji', img.wavelengths[wave1_ind:wave2_ind+1],slope) + np.expand_dims(intercept,axis=2)
+        continuum = np.einsum('i,mj->mji', reflectance.wavelengths[wave1_ind:wave2_ind+1],slope) + np.expand_dims(intercept,axis=2)
 
         depth  = (continuum-chunk)/continuum
         band_area =np.trapz(depth,axis=2)
@@ -117,23 +104,23 @@ def main():
               iterator.current_column:iterator.current_column+chunk.shape[1]] = grain_chunk
         i+=grain_chunk.shape[0]*grain_chunk.shape[1]
         if args.verbose:
-            progbar(i,img.lines*img.columns, full_progbar = 100)
+            progbar(i,reflectance.lines*reflectance.columns, full_progbar = 100)
 
     print('\n')
 
     #Mask pixels outside of bounds
-    grain_size[(grain_size < 60) | (grain_size >= 1000)] = 0
-    grain_size[~img.mask['no_data']] = -9999
+    grain_size[(grain_size < 60) | (grain_size > 1000)] = 0
+    grain_size[~reflectance.mask['no_data']] = -9999
     grain_size[mask] = -9999
 
     # Export grain size map
-    grain_header = img.get_header()
+    grain_header = reflectance.get_header()
     grain_header['bands']= 1
     grain_header['band names']= ['grain_size_um']
     grain_header['wavelength']= []
     grain_header['fwhm']= []
 
-    out_file = args.out_dir + img.base_name + '_grainsize'
+    out_file = args.out_dir + reflectance.base_name + '_grainsize'
     writer = WriteENVI(out_file,grain_header)
     writer.write_band(grain_size,0)
 
