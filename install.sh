dd239b82e1b01fcd9ff6bc3c20a29d708531bb58
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2024-01-26 15:03:08 -0800

    Use source activate

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ pge_dir=$(cd "$(dirname "$0")" ; pwd -P)
 
 # Need to do custom install to prevent dependency errors
 conda create -y --name sister python=3.8
-conda activate sister
+source activate sister
 
 conda install gdal -y
 pip install pystac==1.8.4
